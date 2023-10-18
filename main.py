2b1da925dec310f37856003e882b673a15a89d33
alexmavromates@gmail.com <alexmavromates@gmail.com>
2023-10-18 16:22:09 +0100

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+class Matric:
+    def __init__(self):
+        # Initialization code here
+        pass
+
+    def method1(self):
+        # Placeholder for a method
+        print("Method 1 executed!")
+
+    def method2(self):
+        # Placeholder for another method
+        print("Method 2 executed!")
+
+def main():
+    matric_instance = Matric()
+    matric_instance.method1()
+    matric_instance.method2()
+
+if __name__ == "__main__":
+    main()
