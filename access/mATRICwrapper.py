cb31c61a0eb291e4600fa8a8e6eb1f3e23896b42
mavsgavs <alexmavromates@gmail.com>
2023-11-11 22:40:13 +0000

    nokia ready with timestamp and matric id

--- access/mATRICwrapper.py
+++ access/mATRICwrapper.py
@@ -1,3 +1,4 @@
+# this code will generate 5G Nokia CLI data in json format
 import os
 import json
 import random
@@ -7,7 +8,7 @@ from datetime import datetime
 # Define the path to your JSON structure file
 json_structure_file = './Nokia.json'
 
-# Function to generate random time strings
+# Function to generate random time strings for the actualStartTime and actualStopTime
 def generate_random_time():
     """Generate a random time string in the format of 'YYYY-MM-DDTHH:MM:SSZ'."""
     year = random.randint(2020, 2023)
@@ -17,6 +18,11 @@ def generate_random_time():
     minute = random.randint(0, 59)
     second = random.randint(0, 59)
     return f"{year:04d}-{month:02d}-{day:02d}T{hour:02d}:{minute:02d}:{second:02d}Z"
+
+# Function to generate a unique identifier for matricID
+def generate_matric_id():
+    """Generate a random matricID string."""
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
 
 # APManager class definition
 class APManager:
@@ -29,7 +35,7 @@ class APManager:
             raise FileNotFoundError(f"The structure file {structure_file_path} does not exist.")
 
     def getAPdata(self, ap_type):
-        """Generates a JSON object based on the AP type."""
+        """Generates a JSON object with a timestamp and matricID based on the AP type."""
         if ap_type.lower() == '5g':
             # Populate the 5G structure with random data
             data = self.populate_5g_data()
@@ -38,11 +44,17 @@ class APManager:
             data = {}
         else:
             raise ValueError("AP type must be '5g', 'wifi', or 'lifi'.")
+        
+        # Add the current timestamp and matricID to the data
+        data['timestamp'] = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+        data['matricID'] = generate_matric_id()
+        
         return data
 
     def populate_5g_data(self):
         """Populates the 5G JSON structure with random data."""
         data = json.loads(json.dumps(self.json_5g_structure))
+        
         # Populate the JSON structure with random data
         data["distName"] += ''.join(random.choices(string.ascii_letters, k=5))
         data["cellId"] = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
@@ -52,6 +64,7 @@ class APManager:
         data["results"]["fwdTestDisp"] += ''.join(random.choices(string.ascii_letters, k=3))
         data["results"]["result"] += ''.join(random.choices(string.ascii_letters, k=3))
         
+        # Populate the rfAntResults with random data
         rf_ant_results = data["results"]["rfAntResults"]
         rf_ant_results["antId"] = random.randint(1, 10)
         rf_ant_results["fwdETPw"] = random.uniform(0, 100)
@@ -65,6 +78,7 @@ class APManager:
         rf_ant_results["rsi"] = random.uniform(-100, 0)        # Assuming NR only
         rf_ant_results["sinr"] = random.uniform(0, 30)         # Assuming NR only
         
+        # Populate the rest of the fields with random data
         data["results"]["rvrsTestDisp"] += ''.join(random.choices(string.ascii_letters, k=3))
         data["results"]["testOutcome"] += ''.join(random.choices(string.ascii_letters, k=3))
         data["results"]["testOutcomeAdditionalInfo"] += ''.join(random.choices(string.ascii_letters, k=10))
@@ -73,13 +87,14 @@ class APManager:
         return data
 
     def pubAPdata(self, json_data):
-        """Prints the provided JSON object."""
+    # the wiremq implementation to publish the json should be implemented here 
         print("AP Data:", json.dumps(json_data, indent=4))
 
 # Example usage
 ap_manager = APManager(json_structure_file)
 
-# Get and print data for different AP types
-for ap_type in ['5g', 'wifi', 'lifi']:
-    ap_data = ap_manager.getAPdata(ap_type)
-    ap_manager.pubAPdata(ap_data)
+# Get and print data for a 5G access point
+ap_data_5g = ap_manager.getAPdata('5g')
+ap_manager.pubAPdata(ap_data_5g)
+
+# The Wi-Fi and Li-Fi data generation can be implemented similarly when their structures are defined.
